ebcca633fba8a335166bb157a903c2e31970289e
syuntoku14 <syuntoku14@gmail.com>
2021-03-09 21:50:28 +0900

    Update: Add githubpages extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,8 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
